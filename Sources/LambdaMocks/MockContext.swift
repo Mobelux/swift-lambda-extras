5035895e392495dec93a38ac522f763e4eb90e21
Matt Gacy <matt@mgacy.com>
2024-01-19 14:29:26 -0700

    Improve ergonomics of MockContext creation

--- Sources/LambdaMocks/MockContext.swift
+++ Sources/LambdaMocks/MockContext.swift
@@ -82,34 +82,90 @@ public struct MockContext<E>: RuntimeContext, EnvironmentValueProvider {
 }
 
 public extension MockContext {
+
+    /// Configuration data for ``MockContext``.
+    struct Configuration {
+        /// The request ID, which identifies the request that triggered the function invocation.
+        public var requestID: String
+
+        /// The AWS X-Ray tracing header.
+        public var traceID: String
+
+        /// The ARN of the Lambda function, version, or alias that's specified in the invocation.
+        public var invokedFunctionARN: String
+
+        /// The time interval before the context's deadline.
+        public var timeout: DispatchTimeInterval
+
+        /// For invocations from the AWS Mobile SDK, data about the Amazon Cognito identity provider.
+        public var cognitoIdentity: String?
+
+        /// For invocations from the AWS Mobile SDK, data about the client application and device.
+        public var clientContext: String?
+
+        /// Creates an instance.
+        ///
+        /// - Parameters:
+        ///   - requestID: The request ID.
+        ///   - traceID: The AWS X-Ray tracing header.
+        ///   - invokedFunctionARN: The ARN of the Lambda function.
+        ///   - timeout: The time interval before the context's deadline.
+        ///   - cognitoIdentity: Data about the Amazon Cognito identity provider.
+        ///   - clientContext: Data about the client application and device.
+        public init(
+            requestID: String = "\(DispatchTime.now().uptimeNanoseconds)",
+            traceID: String = "Root=\(DispatchTime.now().uptimeNanoseconds);Parent=\(DispatchTime.now().uptimeNanoseconds);Sampled=1",
+            invokedFunctionARN: String = "arn:aws:lambda:us-east-1:\(DispatchTime.now().uptimeNanoseconds):function:custom-runtime",
+            timeout: DispatchTimeInterval = .seconds(5),
+            cognitoIdentity: String? = nil,
+            clientContext: String? = nil
+        ) {
+            self.requestID = requestID
+            self.traceID = traceID
+            self.invokedFunctionARN = invokedFunctionARN
+            self.timeout = timeout
+            self.cognitoIdentity = cognitoIdentity
+            self.clientContext = clientContext
+        }
+    }
+
+    /// Returns the time interval between a given point in time and the current time.
+    ///
+    /// - Parameter deadline: The time with which to compare now.
+    /// - Returns: The time interval between the given deadline and now.
+    @Sendable
+    static func timeAmountUntil(_ deadline: DispatchWallTime) -> TimeAmount {
+        .milliseconds(deadline.millisecondsSinceEpoch - DispatchWallTime.now().millisecondsSinceEpoch)
+    }
+
     /// Creates a new instance.
     ///
     /// - Parameters:
-    ///   - timeout: The time interval at which the function will time out.
-    ///   - requestID: The request ID.
-    ///   - traceID: The tracing header.
-    ///   - invokedFunctionARN: The ARN of the Lambda function.
     ///   - eventLoop: The event loop.
+    ///   - configuration: The context configuration.
+    ///   - logger: The logger.
     ///   - allocator: The byte buffer allocator.
+    ///   - remainingTimeProvider:
     ///   - environmentValueProvider: A closure returning the value of the given environment
     ///   variable.
     init(
-        timeout: DispatchTimeInterval = .seconds(3),
-        requestID: String = UUID().uuidString,
-        traceID: String = "abc123",
-        invokedFunctionARN: String = "aws:arn:",
-        logger: Logger = .mock,
         eventLoop: EventLoop,
+        configuration: Configuration = .init(),
+        logger: Logger = .mock,
         allocator: ByteBufferAllocator = .init(),
+        remainingTimeProvider: @escaping @Sendable (DispatchWallTime) -> TimeAmount = Self.timeAmountUntil,
         environmentValueProvider: @escaping @Sendable (E) throws -> String
     ) {
-        self.requestID = requestID
-        self.traceID = traceID
-        self.invokedFunctionARN = invokedFunctionARN
-        self.deadline = .now() + timeout
+        self.requestID = configuration.requestID
+        self.traceID = configuration.traceID
+        self.invokedFunctionARN = configuration.invokedFunctionARN
+        self.deadline = .now() + configuration.timeout
+        self.cognitoIdentity = configuration.cognitoIdentity
+        self.clientContext = configuration.clientContext
         self.logger = logger
         self.eventLoop = eventLoop
         self.allocator = allocator
+        self.remainingTimeProvider = remainingTimeProvider
         self.environmentValueProvider = environmentValueProvider
     }
 }
